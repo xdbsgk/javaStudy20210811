60fb3098f40858663e953d9afbb775763addcef0
= <=>
2021-08-18 21:11:33 +0900

    remove results

--- 김윤하0129/src/a7_조건/Quiz2753.java
+++ 김윤하0129/src/a7_조건/Quiz2753.java
@@ -11,10 +11,8 @@ public class Quiz2753 {
 		int result;
 		
 		if (year%4==0 && year%100!=0 || year%400==0) {
-			result = 1;
 			System.out.println("¿±≥‚¿‘¥œ¥Ÿ.!!");
 		}else {
-			result = 0;
 			System.out.println("¿±≥‚¿Ã æ∆¥’¥œ¥Ÿ.!!");
 		}
 		

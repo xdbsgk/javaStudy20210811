6dee1be52356a8d40decaa88406ba25cb6fb2c26
윤하 <@DESKTOP-91JMON1>
2021-08-26 23:44:35 +0900

    퀴즈 2차 풀이

--- 김윤하0129/src/a9_메소드/Quiz.java
+++ 김윤하0129/src/a9_메소드/Quiz.java
@@ -4,38 +4,69 @@ import java.util.Scanner;
 
 public class Quiz {
 	
-	String quiz1(int sNum) {
-		String res1 = "?";
-		return res1;
+	static void quiz1(int starNum) {					// 1번 문제 메소드
+		for (int i=0; i<starNum; i++) {
+			int i2=i+1;
+			for(int j=0; j<i2; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			}
 	}
 	
-	String quiz2(int starNum) {
-		String res2 = "?";
-		return res2;
+	static void quiz2(int starNum) {					// 2번 문제 메소드
+		for (int i=0; i<starNum; i++) {
+			for(int j=0; j<starNum-i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 	
-	String quiz3(int starNum) {
-		String res3 = "?";
-		return res3;
+	static void quiz3(int starNum) {					// 3번 문제 메소드
+		for (int i=0; i<starNum; i++) {
+			int i2=i+1;
+			for(int j=0; j<starNum-i2; j++) {
+				System.out.print(" ");
+			}
+			for (int j=0; j<i2; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 	
-	String quiz4(int starNum) {
-		String res4 = "?";
-		return res4;
+	static void quiz4(int starNum) {					// 4번 문제 메소드
+		for (int i=0; i<starNum; i++) {
+			for(int j=0; j<i; j++) {
+				System.out.print(" ");
+			}
+			for (int j=0; j<starNum-i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 	
-	String quiz5(int starNum) {
-		String res5 = "?";
-		return res5;
+	static void quiz5(int starNum) {					// 5번 문제 메소드
+		for (int i=0; i<starNum; i++) {
+			int i2= (2*i)+1;
+			for(int j=0; j<starNum-i; j++) {
+				System.out.print(" ");
+			}
+			for (int j=0; j<i2; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
+
 	
 	public static void main(String[] args) {
 		
 		Scanner s = new Scanner(System.in);				// Scanner
 		
-		boolean ctrl = true;							// 전체 컨트롤 변수 선언
-		
-		while(ctrl) {									// 프로그램 실행
+		while(true) {									// 프로그램 실행
 			
 			System.out.println("[별 찍기 프로그램]");		// 메뉴
 			System.out.println("1. 문제 1");
@@ -45,28 +76,48 @@ public class Quiz {
 			System.out.println("5. 문제 5");
 			System.out.println("q. 프로그램 종료");
 			System.out.print("명령을 입력하세요: ");		// 명령 입력
-			String selecter = s.nextLine();				// 문자열 입력(1~5,q)
+			String selecter = s.nextLine();				// 문자열 입력 (1~5, q, etc)
 			System.out.println();
 			
-			if (selecter.equals("1") || selecter.equals("2") || selecter.equals("3") || selecter.equals("4") || selecter.equals("5")) {
-				
+		
+			if (selecter.equals("1")) {
 				System.out.println("출력할 별(*)의 개수를 입력하세요.: ");
 				int starNum = s.nextInt();
-				
-//				String procStart(selecter, starNum) {
-//					System.out.println();
-//			}
-				
-				
+				quiz1(starNum);
+				break;
+			}else if(selecter.equals("2")) {
+				System.out.println("출력할 별(*)의 개수를 입력하세요.: ");
+				int starNum = s.nextInt();
+				quiz2(starNum);
+				break;
+			}else if(selecter.equals("3")) {
+				System.out.println("출력할 별(*)의 개수를 입력하세요.: ");
+				int starNum = s.nextInt();
+				quiz3(starNum);
+				break;
+			}else if(selecter.equals("4")) {
+				System.out.println("출력할 별(*)의 개수를 입력하세요.: ");
+				int starNum = s.nextInt();
+				quiz4(starNum);
+				break;
+			}else if(selecter.equals("5")) {
+				System.out.println("출력할 별(*)의 개수를 입력하세요.: ");
+				int starNum = s.nextInt();
+				quiz5(starNum);
+				break;
 			}else if (selecter.equals("q")) {
 				System.out.println("프로그램 종료중 ...");
 				break;
+				
 			}else {
 				System.out.println("잘못된 입력입니다. 다시 입력해 주세요.");
 			}
 			
 			}
+		
+		System.out.println();
 		System.out.println("프로그램 종료");
+	
 	}
 
 

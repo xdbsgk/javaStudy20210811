585c14226703f93b9cdc7cfe82c0d96889c4a64f
= <=>
2021-09-08 18:50:06 +0900

    Quiz 10818 추가 (최소, 최대 - 배열)

---
+++ 김윤하0129/src/b11_배열/Quiz10818.java
@@ -0,0 +1,28 @@
+package b11_배열;
+
+import java.util.Scanner;
+
+public class Quiz10818 {
+// 최소, 최대 (배열)
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		int nCount = in.nextInt();
+		int[] nArr;
+		nArr = new int[nCount];
+
+		int maxNum = -1000001;
+		int minNum = 1000001;
+		
+		for(int i=0; i<nCount; i++) {
+			nArr[i] = in.nextInt();
+			
+			if (nArr[i]<minNum) minNum = nArr[i];
+			if (nArr[i]>maxNum) maxNum = nArr[i];
+		}
+		
+		System.out.println(minNum + " " + maxNum);
+	}
+
+}
